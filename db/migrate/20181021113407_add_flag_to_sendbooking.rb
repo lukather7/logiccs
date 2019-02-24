9c515e086e8b0a23a9f154ef7eb0f21c169d6e80
fumiohosoya <fumioh@cscoms.com>
2019-02-24 11:33:14 +0000

    add_flag_to_sendbooking set default to false

--- db/migrate/20181021113407_add_flag_to_sendbooking.rb
+++ db/migrate/20181021113407_add_flag_to_sendbooking.rb
@@ -1,5 +1,5 @@
 class AddFlagToSendbooking < ActiveRecord::Migration
   def change
-    add_column :sendbookings, :done, :boolean, default: 0
+    add_column :sendbookings, :done, :boolean, default: false
   end
 end
